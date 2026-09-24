4079227edfe86d68219315ddb3212013dbd3fe78
TROMBONEGEEK <wade36760@gmail.com>
2026-09-24 14:20:16 +0000

    I added the challenge file for HW I finished the "hello-world" file

--- tutorials/hello-world.cs
+++ tutorials/hello-world.cs
@@ -26,3 +26,15 @@ Console.WriteLine($"[{trimmedGreeting}]");
 
 trimmedGreeting = greeting.Trim();
 Console.WriteLine($"[{trimmedGreeting}]");
+
+
+string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+
+string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.Contains("goodbye"));
+Console.WriteLine(songLyrics.Contains("greetings"));
